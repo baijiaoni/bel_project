f52deb225d2196737e9fd41a2f8cbc68409b786d
Bai Jianoi <baijn@ihep.ac.cn>
2014-03-07 14:07:19 +0100

    test

--- modules/butis/pexaria.vhd
+++ modules/butis/pexaria.vhd
@@ -30,12 +30,14 @@ ARCHITECTURE LogicFunction OF pexaria_e IS
    signal r_ack      : std_LOGIC := '0';
    signal r_err      : std_LOGIC := '0';	
    signal r_stall    : std_LOGIC := '0';
-   signal r_dato     : std_LOGIC := '0';
+   signal r_dato     : t_wishbone_data;
    signal r_echo     : t_wishbone_data;
-   
-   constant c_CNT    : natural   := 0;
-   constant c_CLR    : natural   := c_CNT +4:
-   constant c_ECHO   : natural   := c_CLR +4;
+   signal r_clr      : std_logic_vector(0 downto 0);   
+
+   constant c_CNT    : natural   := 0;         -- Read Counter, ro  0x00 
+   constant c_CLR    : natural   := c_CNT +4;  -- Clear counter, wo 0x04
+   constant c_ECHO   : natural   := c_CLR +4;  -- Echo register, rw 0x08
+ 
 
 
 
@@ -56,25 +58,29 @@ BEGIN
    begin
       if rising_edge(clk_sys_i) then
          if rst_n_i = '0' then
-         
+            r_echo   <= x"deadbeef";
+            r_stall  <= '0';
+            r_ack    <= '0';
+            r_err    <= '0';
          else
             v_en  := slave_i.cyc and slave_i.stb and not r_stall;
             v_adr := to_integer(unsigned(slave_i.adr(7 downto 0)));
             v_we  := slave_i.we;
             r_dato <= (others => '0');
+            r_clr  <= "0";
 
             if v_en = '1' then
                r_ack <= '1';
                if v_we ='1' then
                   case v_adr is
-                     when c_CLR  => r_dat   => counterr;
-                     when c_ECHO => r_dato   <= r_echo;
+                     when c_CLR  => r_clr    <= f_wb_wr(r_clr,  slave_i.dat, slave_i.sel, "set");
+                     when c_ECHO => r_echo   <= f_wb_wr(r_echo, slave_i.dat, slave_i.sel, "owr");
                      when others => r_ack    <= '0'; r_err <= '1';
                   end case;
                else                
                   case v_adr is
-                     when c_CNT  => r_dato(counterr'range) <= counterr;
-                     when c_ECHO => r_dato(r_echo'range)   <= r_echo;
+                     when c_CNT  => r_dato(counterror'range) <= std_logic_vector(counterror);
+                     when c_ECHO => r_dato(r_echo'range)     <= r_echo;
                      when others => r_ack    <= '0'; r_err <= '1';
                   end case;
                end if; -- we
@@ -86,7 +92,7 @@ BEGIN
 		counter : process(clk_i) is 
 		BEGIN
 		if rising_edge(clk_i) then 
-			if (lastb = '1' and button_i = '0') then
+			if ((lastb = '1' and button_i = '0') or r_clr(0) = '1') then
 				counterror <= "0000000";
 			end if;	
 			if (last = '1' and io2_i = '0') then
